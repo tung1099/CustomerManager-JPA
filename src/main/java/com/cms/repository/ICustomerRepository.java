6f810a32da22089d7cce4ad2cfde9da61817ea36
tung1099 <dtroyal1102@gmail.com>
2022-04-21 16:06:19 +0700

    first commit

---
+++ src/main/java/com/cms/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.cms.repository;
+
+import com.cms.model.Customer;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
---
+++ src/main/java/com/cms/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.cms.service;
+
+import com.cms.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+
+}
